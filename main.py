b60c990474d01a4a3e1c50bdf601b692cf94bd99
Svenden123 <buhrus1@yandex.ru>
2021-07-29 21:07:16 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+some_string = 'I\'m a programmer'
+#print(some_string)
+another_string = 'I want to learn \"python"'
+#print(another_string)
+
+
+string_with_new_lines = "Hello! \nMy name is Herman"
+#print(string_with_new_lines)
+
+string_with_new_lines1 = "Hello! \n My name is Herman"
+#print(string_with_new_lines1)
+
+string_with_new_lines2 = "Hello! \n \r      My name is Herman"
+print(string_with_new_lines2)
+
+
+numbers = "1\t234\t567890"
+print(numbers)
+
+some_text = "\tHello! \n\tI'm glad to see you!"
+print(some_text)
